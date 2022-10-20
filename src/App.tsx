878fa100de9560f3d386b7c69a8268adbf2f0e51
clark <63522725+theineptdev@users.noreply.github.com>
2022-10-20 00:01:56 -0500

    functional build: - need to finish dark/light mode - WIP threejs cube

---
+++ src/App.tsx
@@ -0,0 +1,71 @@
+import './App.css';
+import Cube from './components/Cube';
+import { useState } from 'react';
+
+import { Canvas } from '@react-three/fiber';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFeather, faEnvelope, faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin ,faTwitter} from '@fortawesome/free-brands-svg-icons';
+
+function App() {
+  const [darkMode, setDarkMode] = useState(true);
+  
+  if (!darkMode) {
+    document.documentElement.style.setProperty('--bgColor', "white")
+    document.documentElement.style.setProperty('--bgColor2', "#f0f0f0")
+    document.documentElement.style.setProperty('--accentColor', "black")
+  } else {
+    document.documentElement.style.setProperty('--bgColor', "black")
+    document.documentElement.style.setProperty('--bgColor2', "#0c0c0c")
+    document.documentElement.style.setProperty('--accentColor', "white")
+  }
+
+  let darkLightIcon = darkMode ? "faSun" : "faMoon";
+
+  return (
+    <div className="app">
+      <Canvas className="canvas">
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[-2, 5, 2]} intensity={1} />
+        <Cube darkMode={darkMode} setDarkMode={setDarkMode}/>
+      </Canvas>
+      <div className="realName">gunner clark</div>
+      <div className="userName">@theineptdev</div>
+      {/* <div className="description">My name is Guner Clark. I'm a software developer</div> */}
+      <div id="links">
+        <a className="link" id="blog" href="https://theinept.blog/" target="_blank">
+          <FontAwesomeIcon icon={faFeather} size="lg" />
+          &nbsp;
+          Blog
+        </a>
+        <a className="link" id="gitHub" href="https://github.com/theineptdev" target="_blank">
+          <FontAwesomeIcon icon={faGithub} size="lg" />
+          &nbsp;
+          GitHub
+        </a>
+        <a className="link" id="linkedIn" href="https://www.linkedin.com/in/gunnerclark" target="_blank">
+          <FontAwesomeIcon icon={faLinkedin} size="lg" />
+          &nbsp;
+          LinkedIn
+        </a>
+        <a className="link" id="twitter" href="https://twitter.com/theineptdev" target="_blank">
+          <FontAwesomeIcon icon={faTwitter} size="lg" />
+          &nbsp;
+          Twitter
+        </a>
+        <a className="link" id="mail" href="mailto:contact@gunnerclark.dev?subject=Website: [Your Subject]" target="_blank">
+          <FontAwesomeIcon icon={faEnvelope} size="lg" />
+          &nbsp;
+          Mail
+        </a>
+      </div>
+      {/* <div className="darkToggleContainer" onClick={(event) => {setDarkMode(!darkMode)}}>
+        <div className="darkToggle">
+          <FontAwesomeIcon icon={darkMode ? faSun : faMoon} size="lg" />
+        </div>
+      </div> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Cube.tsx
@@ -0,0 +1,31 @@
+import { useRef, useState } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Mesh } from 'three';
+
+function Cube({ darkMode, setDarkMode }: any) {
+
+  const mesh = useRef<Mesh>(null);
+
+  useFrame(() => {
+    if (!mesh.current) {
+      return;
+    }
+    mesh.current.rotation.x += 0.01;
+    mesh.current.rotation.y += 0.01;
+  })
+
+  const [hovered, setHover] = useState(false);
+
+  return (
+    <mesh 
+      ref={mesh}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}
+    >
+      <boxBufferGeometry attach="geometry" args={[3, 3, 3]} />
+      <meshLambertMaterial attach="material" color={darkMode ? "white" : "black"}/>
+    </mesh>
+  )
+}
+
+export default Cube;
